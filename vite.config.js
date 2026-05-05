48c11e2df5a9c12e0b5c1a2d42d9a30e7ab26d90
Steve <steve-obrien@users.noreply.github.com>
2026-05-05 14:14:22 +0100

    js and css paths

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [vue(), tailwindcss()],
-	base: process.env.GITHUB_ACTIONS ? '/steve-obrien/' : '/',
+	base: '/',
 });
